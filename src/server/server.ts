3b53f03aa19095e12a6d538597e73a23b6795c56
SkyCoderr <haojiechen1995@hotmail.com>
2022-04-19 23:16:35 +0100

    using ts-node to start the app

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
+// const express = require('express');
 const app = express();
 
 app.use(express.static('@/../dist'));
-app.get(/.*/, (req, res) => res.sendFile('@/../dist/index.html'));
+app.get(/.*/, (req: any, res: any) => res.sendFile('@/../dist/index.html'));
 
 const port = 8000;
 app.listen(process.env.PORT || port, () => console.log(`Server started at localhost:${port}.`));
